f6ef5bfe71c8ad77a02a1d695bb04c6c220f1bb1
theo ounoughi <t.ounoughi@gmail.com>
2022-12-02 13:29:11 -0500

    form create

--- app/controllers/registrations_controller.rb
+++
@@ -1,7 +0,0 @@
-class RegistrationsController < Devise::RegistrationsController
-  protected
-
-  def after_sign_up_path(resource)
-    edit_user_profil_path(@user)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :user, controllers: { registrations: "registrations" }, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
+  devise_for :user, :path => '', :path_names => { :sign_in => "login", :sign_out => "logout", :sign_up => "register" }
 
   root to: 'pages#home'
   get 'about', to: 'pages#about'
