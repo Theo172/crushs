906570f274370205567025aeb50aa606c367e5db
adrien schaffner <adrien.schaffner@gmail.com>
2022-12-12 15:58:54 +0100

    landing page animation

--- app/javascript/controllers/loader_controller.js
+++ app/javascript/controllers/loader_controller.js
@@ -1,23 +1,49 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["loader"]
+  static targets = ["loader", "title", "font-slogan", "btn", "btn-login"]
 
   connect() {
     const loader = document.querySelector('.loader');
     const slider = document.querySelector('.slider');
+    const title = document.querySelector('.title');
+    const slogan = document.querySelector('.font-slogan');
+    const btn = document.querySelector('.btn-ghost');
+    const btn_login = document.querySelector('.btn-login');
     // Event.preventDefaulft()
-    var delayInMilliseconds = 1600; //1 second
-    var delayInMilliseconds1500 = 1500;
+    var delayInMilliseconds = 2000; //1 second
+    var delayInMilliseconds2100 = 2100;
+    var delayInMilliseconds2400 = 2400;
+    var delayInMilliseconds2900 = 2900;
 
     setTimeout(function() {
       //your code to be executed after 1 second
       window.addEventListener ("load", loader.classList.add('fondu-out'))
-    }, delayInMilliseconds1500);
+    }, delayInMilliseconds2100);
 
     setTimeout(function() {
       //your code to be executed after 1 second
       window.addEventListener ("load", slider.classList.add('transform-y-0'))
     }, delayInMilliseconds);
+
+    setTimeout(function() {
+      //your code to be executed after 1 second
+      window.addEventListener ("load", title.classList.add('transform-y-20'))
+    }, delayInMilliseconds2400);
+
+    setTimeout(function() {
+      //your code to be executed after 1 second
+      window.addEventListener ("load", slogan.classList.add('transform-x-20'))
+    }, delayInMilliseconds2400);
+
+    // setTimeout(function() {
+    //   //your code to be executed after 1 second
+    //   window.addEventListener ("load", btn_login.classList.add('transform-y-20'))
+    // }, delayInMilliseconds2900);
+
+    setTimeout(function() {
+      //your code to be executed after 1 second
+      window.addEventListener ("load", btn.classList.add('transform-y-50'))
+    }, delayInMilliseconds2400);
   }
 }
--- app/javascript/controllers/scroll_controller.js
+++ app/javascript/controllers/scroll_controller.js
@@ -1,5 +1,28 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["box", "visibility"]
 
+  connect() {
+    console.log("Hello from scroll controller !")
+  }
+
+  scroll() {
+    const scrolled = window.scrollY;
+    window.addEventListener("scroll", scrolled.classList.remove('visibility'));
+    console.log("hey");
+  };
 }
+// .removeClass
+// connect() {
+//   const loader = document.querySelector('.loader');
+//   const slider = document.querySelector('.slider');
+//   // Event.preventDefaulft()
+//   var delayInMilliseconds = 1600; //1 second
+//   var delayInMilliseconds1500 = 1500;
+
+//   setTimeout(function() {
+//     //your code to be executed after 1 second
+//     window.addEventListener ("load", loader.classList.add('fondu-out'))
+//   }, delayInMilliseconds1500);
+// }
